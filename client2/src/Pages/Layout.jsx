7dde22d5f1db9735aff13cab0d93675133ccdcca
pavankumar k <pavankumarmk0811@gmail.com>
2026-06-25 22:58:50 +0530

    fix loader component

--- client2/src/Pages/Layout.jsx
+++ client2/src/Pages/Layout.jsx
@@ -2,31 +2,32 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import { useSelector } from 'react-redux';
-import loader from '../components/loader';
 import Login from './Login';
 
+const Loader = () => (
+  <div className="flex items-center justify-center min-h-screen bg-black">
+    <div className="w-12 h-12 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
+  </div>
+);
+
 const Layout = () => {
-
-
     const {user, loading} = useSelector(state => state.auth)
 
     if(loading) {
-        return <loader />
+        return <Loader />
     }
 
     return (
       <div>
         {
          user ? (
-        
-        <div className='min-h-screen bg-black'>
-            <Navbar />
-            <Outlet />
-        </div>
+          <div className='min-h-screen bg-black'>
+              <Navbar />
+              <Outlet />
+          </div>
          )
         : <Login />
-    }
-
+        }
       </div>
     );
 };
